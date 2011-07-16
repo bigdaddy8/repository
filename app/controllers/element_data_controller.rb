51c97a04869096c73bcd21f4b7699c0e365be94c
unknown <Rohan@.(none)>
2011-07-16 17:55:32 +0100

    fyuyf

--- app/controllers/element_data_controller.rb
+++ app/controllers/element_data_controller.rb
@@ -3,5 +3,7 @@ class ElementDataController < ApplicationController
   # finding the website 
   # associating elements with the website.
   
-  
+  def new
+	@website = Website.find(params[:website_id])
+	@element_data = @website.
 end
